5966071b2839d2d6133dd82fab72c7ade7fc1dbb
sayed <sayed.jaffarhmm@gmail.com>
2022-12-12 01:50:17 +0300

    New Release

--- app.py
+++ app.py
@@ -1,79 +1,109 @@
-from flask import Flask, render_template, request, redirect, url_for, session
-from flask_cors import cross_origin
-# from flask_session import Session
-from time import sleep
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
-import utilities as ut
+import firebase_admin
+from firebase_admin import credentials, firestore
+
 import pandas as pd
-from zipfile import ZipFile
-import os
+import sys, os, random
+from io import StringIO
 
+#For Production Hosting
+from whitenoise import WhiteNoise
+from waitress import serve
 
 app = Flask(__name__)
-app.secret_key = 'secret_key'
-app.static_folder = 'static'
-app.config['SESSION_TYPE'] = 'filesystem'
+app.wsgi_app = WhiteNoise(app.wsgi_app, root="static/")
 
-@app.route('/home', methods=['GET', 'POST'])
-@app.route('/', methods=['GET', 'POST'])
-@cross_origin()
-def home():
-    if request.method == 'GET':
-        quiz_repo = ut.get_buttons_names()
-        return render_template('home.html', quiz_repo= quiz_repo)
-    
-    else:
-        print(f'Form: {request.form}', flush=True)
-        quiz_id = int(request.form.get('request_quiz_name'))
-        print(f'Quiz_ID: {quiz_id}', flush=True)
-        session['quiz_id'] = quiz_id
+#Initialize Firebase
+cred = credentials.Certificate('firebase-account.json')
+firebase_app = firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-        return redirect(url_for('quiz')) # we want to redirect user to quiz page; passing quiz data through session.
+#Reference to quizes collection
+quizs_collection = db.collection(u'quizes')
+#Reference to history document
+history_document = db.collection(u'history').document(u'history')
 
+#Quizes Data [title {min, max}] -> to get random quiz from chosen collection
+quizz_data = {
+    'AR_GENERAL_VOCABULARY_':{'min':1, 'max':100},
+    'NOUN_QUIZ_':{'min':1, 'max':100},
+    'VERB_QUIZ_':{'min':1, 'max':100}
+}
 
-@app.route('/quiz' , methods=['POST', 'GET'])
-@cross_origin()
-def quiz():
-    if request.method == 'GET':
-        new_quiz = ut.get_new_quiz(session['quiz_id'])
-        kwargs = {
-            'quiz_set_from_flask': new_quiz['quiz_set_from_flask'],
-            'quiz_name_from_flask': new_quiz['quiz_name_from_flask'],
-            'language_from_flask': new_quiz['language_from_flask']
+#Just used one time to send files to the database
+def setup_db(folder):
+    dir = 'quiz_repo/%s' % folder
+    files = [path for path in os.listdir(dir) if os.path.isfile(os.path.join(dir, path))]
+    paths = [os.path.join(dir, path) for path in files if path.find('.csv') > 0]
+    for i,path in enumerate(paths):
+        file = open(path, 'r').read()
+        data = {
+            u'data': u'%s' % file
         }
+        quizs_collection.document(u'%s' % files[i].replace('.csv', '')).set(data)
 
-        return render_template('quiz.html', **kwargs)
-    
+#Allow Cross_origin without extensions
+@app.after_request
+def after_request_func(response):
+	origin = request.headers.get('Origin')
+	if request.method == 'OPTIONS':
+		response = make_response()
+		response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+		response.headers.add('Access-Control-Allow-Origin', origin)
+		response.headers.add('Access-Control-Allow-Methods',
+                                'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+	else:
+		response.headers.add('Access-Control-Allow-Origin', origin)
+	return response
+
+@app.route('/home')
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+#POST -> Save History | GET -> Get History
+@app.route('/history', methods=['POST', 'GET'])
+def save_history():
+    h = history_document.get()
+    if request.method == 'GET':
+        if h.exists:
+            return jsonify(h.to_dict())
+        else:
+            return jsonify({})
     else:
-        quiz_date = ut.get_datetime_now()
-        q_results = request.form.to_dict()
+        if h.exists:
+            h = h.to_dict()
+            for k in h.keys():
+                h[k].insert(0, u'%s' % request.json[k])
+        else:
+            h = {
+                u'score': [ u'%s' % request.json['score'] ],
+                u'correct': [ u'%s' % request.json['correct'] ],
+                u'wrong': [ u'%s' % request.json['wrong'] ]
+            }
+        history_document.set(h)
+        return jsonify(h)
 
-        mistakes = [v for k,v in q_results.items() if k.startswith('wrong_answer')]
+#Get random quiz from the database
+@app.route('/quiz/<collection>', methods=['GET'])
+def get_random_quiz(collection):
+    if collection is None or not collection in quizz_data:
+        return jsonify({})
+    else:
+        r = random.randint(quizz_data[collection]['min'], quizz_data[collection]['max'])
+        doc = quizs_collection.document(f'{collection}{r:03d}').get()
+        if doc.exists:
+            csvStringIO = StringIO(doc.to_dict()['data'])
+            return jsonify(pd.read_csv(csvStringIO).to_dict())
+        else:
+            return jsonify({})
 
-        quiz_result = {}
+@app.route('/data', methods=['GET'])
+def get_data():
+    return jsonify(quizz_data)
 
-        print(f'Mistakes: {mistakes}, {type(mistakes)}', flush=True)
-
-        quiz_result = {k:v for k,v in q_results.items() if ((k != 'quiz_result')  and (k.startswith('wrong_answer') == False))}
-        print(f'Quiz Result: {quiz_result}', flush=True)
-        quiz_result['date_time'] = quiz_date
-        quiz_result['mistakes'] = mistakes
-
-        ut.update_json_file_with_dict('quizzes_result.json', quiz_result)
-
-        return redirect(url_for('home'))
-
-@app.route('/quizzes_history')
-@cross_origin()
-def quizzes_history():
-    try:
-        table_from_flask = ut.make_html_table('quizzes_result.json')
-        return render_template('quizzes_history.html', table_from_flask=table_from_flask)
-
-    except:
-        return render_template ('quizzes_history.html')
-
-
-
+#Serving with waitress
 if __name__ == '__main__':
-    app.run(debug=True)
+    print('Serving on http://localhost:8000/')
+    serve(app, host='0.0.0.0', port=8000)
--- static/app.js
+++ static/app.js
@@ -1,189 +1,122 @@
-// const quiz_set = document.getElementById('quiz_set_from_flask')
-let quiz_set = document.getElementById('quiz_set_from_flask').textContent
-quiz_set = JSON.parse(quiz_set)
+let quiz = null;
+let current_answer = 0;
+let max = 0;
+let history = {
+    score: 0,
+    correct: 0,
+    wrong: 0
+};
 
-let quiz_name = document.getElementById('quiz_name_from_flask').textContent.trim();
-console.log(quiz_name)
+const views = [
+    document.getElementById('home'),
+    document.getElementById('quiz'),
+    document.getElementById('history')
+];
 
-const start_of_quiz = new Date();
-var end_of_quiz;
-var duration_of_quiz_minutes; // global variable declarations for duration of quiz in minutes;
+const userinput = document.getElementById('input');
+const minAnswer = document.getElementById('minAnswer');
+const maxAnswer = document.getElementById('maxAnswer');
+const question = document.getElementById('question');
+const score = document.getElementById('score');
+const correct = document.getElementById('correct');
+const wrong = document.getElementById('wrong');
+const table = document.getElementById('table');
 
-console.log(quiz_set)
+ready();
+async function ready() {
+    let response = await fetch('/data', {mode: 'cors',method: 'GET'});
+    let json = await response.json();
 
-const quiz_length = quiz_set.length;
+    let inner = '';
+    Array.from(Object.keys(json)).forEach((k) => {
+        inner += `<a onclick="getQuiz('${k}')" class="button">${k}</a>`
+    });
+    inner += `<a onclick="getHistory()" class="button">HISTORY</a>`
+    document.getElementById('home').innerHTML = inner;
 
-const ui_start = document.getElementById('start_game')
-const ui_quiz_container = document.getElementById('quiz_container')
-const ui_question_text = document.getElementById('question_text')
-const ui_quiz_no = document.getElementById('quiz_no')
-
-const ui_correct_answers = document.getElementById('correct_answers')
-const ui_wrong_answers = document.getElementById('wrong_answers')
-const ui_score = document.getElementById('score')
-const ui_feedback = document.getElementById('feedback')
-
-const ui_answer_text = document.getElementById('answer_area')
-const ui_submit_answer = document.getElementById('submit_button')
-
-const ui_progress_bar = document.getElementById('prog_bar')
-const ui_result = document.getElementById('result')
-
-var quiz_number = 0
-var choices;
-
-var correct_answers = 0;
-var wrong_answers = 0;
-var score = 0;
-
-var progress = 0;
-var set_time_out;
-
-console.log('Language:', `${language}`)
-if (language == 'ar') {
-    ui_quiz_no.innerText = `السؤال ${quiz_number} من ${quiz_length}`;
-    ui_answer_text.placeholder = 'الإجابة';
-    ui_submit_answer.value = 'تأكيد';
-}
-
-
-
-ui_start.addEventListener('click', startGame)
-ui_submit_answer.addEventListener('click', submitAnswer)
-
-ui_answer_text.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.key === "Enter") {
-        ui_submit_answer.click();
-    }
-  });
-
-
-let someArray = [];
-
-function startGame() {
-    console.log('Game Started')
-    ui_start.classList.add('hide')
-    ui_quiz_container.classList.remove('hide')
-    makeQuiz()
-}
-
-function gameOver() {
-    console.log('Game Over')
-    end_of_quiz = new Date();
-    duration_of_quiz_minutes = (end_of_quiz - start_of_quiz) / 60000;
-    makeForm(someArray, 'post_form_flask_01')
-    ui_quiz_container.classList.add('hide')    
-}
-
-function makeQuiz() {
-    if (quiz_set.length > 0) {
-        let currenQuiz = quiz_set.shift();
-        quiz_number = quiz_number + 1;
-        
-        if (language == 'ar') {
-            ui_quiz_no.textContent = `السؤال ${quiz_number} من ${quiz_length}`}
-        else {
-        ui_quiz_no.textContent = `Question ${quiz_number} of ${quiz_length}`;}
-        ui_question_text.textContent = currenQuiz.QUESTION_TEXT;
-
-        choices = [currenQuiz.OPTION_1, currenQuiz.OPTION_2, currenQuiz.OPTION_3]
-    } else {gameOver()}
-    
-
-}
-
-function submitAnswer() {
-    let answer = ui_answer_text.value.trim()
-    console.log(`Answer in Text Area: ${answer}`)
-    if (choices.includes(answer)) {
-        console.log('Correct', `Indeed ${answer} is one of the choices in ${choices}`)
-        correct_answers = correct_answers + 1;
-        set_time_out = 0;
-    } else {console.log('WRONG;', `Answer ${answer} is not one of the choices in ${choices}`)
-            
-            someArray.push(choices[0]) 
-            wrong_answers = wrong_answers + 1;
-
-            ui_feedback.classList.add('animate_wrong')
-            ui_feedback.textContent = `${choices[0]}`
-            set_time_out = 2000;
-
+    userinput.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          Answer();
         }
-    score = Math.floor(100 * ((correct_answers)/(correct_answers + wrong_answers)));
-
-    ui_correct_answers.textContent = `Correct Answers: ${correct_answers}`
-    ui_wrong_answers.textContent = `Wrong Answers: ${wrong_answers}`
-    ui_score.textContent = `Score: ${score}%`
-    ui_answer_text.value = ''
-    
-
-    progress = Math.floor(100 * ((correct_answers + wrong_answers) / quiz_length))
-    ui_progress_bar.ariaValueNow = `${progress}`
-    ui_progress_bar.ariaValueText = `${progress}%`
-    ui_progress_bar.style.width = `${progress}%`
-    ui_result.scrollIntoView();
-    ui_feedback.addEventListener('animationend', () => {
-        ui_feedback.classList.remove('animate_wrong')
-        ui_feedback.textContent = ''
-    })
-    
-    setTimeout(() => {
-        window.scrollTo(0, 0)
-        makeQuiz()
-    }, set_time_out);
-    
+    }); 
 }
 
-function makeForm (array_01, form_id) {
-    const ui_form_01 = document.getElementById(form_id)
-    for (var i = 0; i < array_01.length; i++) {
-        var input = document.createElement("input");
-        input.type = "hidden";
-        input.value = array_01[i];
-        input.name = "wrong_answer" + (i+1);
-        ui_form_01.appendChild(input);
+function ShowView(id) {
+    views.forEach( v => v.style.display = 'none' );
+    document.getElementById(id).style.display = '';
+}
+
+async function getQuiz(collection) {
+    let response = await fetch(`/quiz/${collection}`, {mode: 'cors',method: 'GET'});
+    let json = await response.json();
+    quiz = json;
+    ShowView('quiz');
+    history.score = history.wrong = history.correct = current_answer = 0;
+    max = Object.keys(quiz['QUESTION_TEXT']).length;
+    maxAnswer.innerText = max;
+    NextAnswer();
+}
+
+async function Answer() {
+    let v = userinput.value;
+    if(v === quiz.OPTION_1[current_answer -1] || v === quiz.OPTION_2[current_answer -1] || v === quiz.OPTION_3[current_answer -1])
+        history.correct++;
+    else
+        history.wrong++;
+    history.score = (history.correct * 100) / max;
+    NextAnswer();
+}
+
+function NextAnswer() {
+    userinput.value = '';
+    current_answer++;
+    if(current_answer == max+1) {
+        saveHistory();
+        return;
     }
-
-    var i_quiz_name = document.createElement("input");
-    i_quiz_name.type = "hidden";
-    i_quiz_name.value = quiz_name;
-    i_quiz_name.name = "quiz_name";
-    ui_form_01.appendChild(i_quiz_name);
-    
-    
-    var i_quiz_result = document.createElement("input");
-    i_quiz_result.type = "hidden";
-    i_quiz_result.value = "quiz_result";
-    i_quiz_result.name = "quiz_result";
-    ui_form_01.appendChild(i_quiz_result);
-
-
-    var i_quiz_duration = document.createElement("input");
-    i_quiz_duration.type = "hidden";
-    i_quiz_duration.value = Math.floor(duration_of_quiz_minutes);
-    i_quiz_duration.name = "quiz_duration_minutes";
-    ui_form_01.appendChild(i_quiz_duration);
-
-
-    var i_no_wrong = document.createElement("input");
-    i_no_wrong.type = "hidden";
-    i_no_wrong.value =  wrong_answers;
-    i_no_wrong.name = "number_of_wrong_answers";
-    ui_form_01.appendChild(i_no_wrong);
-
-    var i_no_correct = document.createElement("input");
-    i_no_correct.type = "hidden";
-    i_no_correct.value =  correct_answers;
-    i_no_correct.name = "number_of_wrong_answers";
-    ui_form_01.appendChild(i_no_correct);
-
-
-    var i_btn = document.createElement("input");
-    i_btn.type = "submit";
-    i_btn.value = "Submit";
-    ui_form_01.appendChild(i_btn);
-    ui_form_01.submit();
-    console.log('Form Submitted')
+    minAnswer.innerText = current_answer;
+    question.innerText = quiz['QUESTION_TEXT'][current_answer - 1];
+    score.innerText = history.score;
+    correct.innerText = history.correct;
+    wrong.innerText = history.wrong;
+    userinput.focus();
 }
 
+async function getHistory() {
+    let response = await fetch(`/history`, {mode: 'cors',method: 'GET'});
+    let json = await response.json();
+    ShowView('history');
+    RenderHistory(json);
+}
+
+async function saveHistory() {
+    let h = JSON.stringify(history);
+    let response = await fetch(`/history`, {mode: 'cors', method: 'POST', headers:{
+        'Content-Type':'application/json',
+        'Content-Length': h.length + 1000
+    }, body: h});
+    let json = await response.json();
+    ShowView('history');
+    RenderHistory(json);
+}
+
+function RenderHistory(data) {
+    inner = `
+        <tr>
+            <th>Score</th>
+            <th>Correct</th>
+            <th>Wrong</th>
+        </tr>
+    `;
+    data['score'].forEach((v, i) => {
+        inner += `
+            <tr>
+                <td>${v}%</td>
+                <td>${data['correct'][i]}</td>
+                <td>${data['wrong'][i]}</td>
+            </tr>
+        `;
+    });
+    table.innerHTML = inner;
+}
